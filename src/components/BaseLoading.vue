0b4a835b98184ae5a379d94944e73da25625848f
jgersain <jhersein@gmail.com>
2021-11-21 00:16:08 -0600

    Add loading component

---
+++ src/components/BaseLoading.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .loading-wrapper.text-center.py-5
+    .spinner-wrapper.w-100
+      b-spinner.spinner(label="Loading..." :type="type" :variant="variant")
+    h1.my-5
+      | Loading...
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    variant: {
+      required: false,
+      type: String,
+      default: 'info',
+      validator: (value) => {
+        return [
+          'primary',
+          'secondary',
+          'danger',
+          'warning',
+          'success',
+          'info',
+          'light',
+          'dark'
+        ].indexOf(value) !== -1
+      }
+    },
+    type: {
+      required: false,
+      type: String,
+      default: 'border',
+      validator: (value) => {
+        return ['border', 'grow'].indexOf(value) !== -1
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .spinner-wrapper
+    .spinner
+      width 4rem
+      height 4rem
+</style>
