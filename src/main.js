0fdbb7a1ffe87736b6ee8cd905a62bce46161947
jgersain <jhersein@gmail.com>
2021-11-21 02:09:13 -0600

    Refactor code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-// NPM packages
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+
+// Bootstrap vue
+import './plugins/bootstrapVue'
 
 // Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './plugins/fontAwesome'
 
 // Local files
 import App from './App.vue'
@@ -14,28 +12,10 @@ import router from './router'
 import store from './store'
 
 // CSS Global
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.styl'
 
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
 // Extra configuration
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
