ad95dabdcab0f12fcdafa9c4fc427b96c92b935e
jgersain <jhersein@gmail.com>
2021-11-21 02:05:27 -0600

    Add header and footer + fontawesome

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,19 @@
 <template lang="pug">
   .container
+    header-bar
     router-view
+    footer-bar
 </template>
 
 <script>
+import HeaderBar from '@/components/HeaderBar/Index'
+import FooterBar from '@/components/FooterBar/Index'
+
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  components: {
+    HeaderBar,
+    FooterBar
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 // NPM packages
-import BootstrapVue from 'bootstrap-vue'
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // Local files
 import App from './App.vue'
@@ -12,9 +18,24 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.styl'
 
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+)
+
 // Extra configuration
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
